e2bedb23f4e0b3f2375ab4b192b6a5c03c77666e
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-03-04 16:45:49 +0300

    checkbox component has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'three-state-checkbox-lib-app';
+  public disabled = false;
+  public id = '1';
+  public required = true;
+  public tabIndex = 1;
+  public isChecked = 2;
 }
