f20f3d947ed8436e8e76bd2468168a49e979834c
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2019-03-04 17:58:46 +0300

    Refactoring (#6)
    * v0.0.2
    
    * v0.0.3
    
    * v0.0.4
    
    * rename files/add next prefix

---
+++ projects/next-three-state-checkbox/src/lib/next-three-state-checkbox.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {NextThreeStateCheckboxComponent} from './components/next-three-state-checkbox/next-three-state-checkbox.component';
+
+@NgModule({
+  declarations: [NextThreeStateCheckboxComponent],
+  exports: [NextThreeStateCheckboxComponent],
+})
+export class NextThreeStateCheckboxModule {}
--- projects/three-state-checkbox-lib/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of three-state-checkbox-lib
- */
-
-export * from './lib/three-state-checkbox-lib.module';
-export * from './lib/components/three-state-checkbox/three-state-checkbox.component';
