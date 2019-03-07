ae072a1afc433e8cab2ef9288d65bf6513267251
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-03-07 13:55:14 +0300

    add tests for reactive form

--- projects/next-three-state-checkbox/src/lib/components/next-three-state-checkbox/next-three-state-checkbox.component.spec.ts
+++ projects/next-three-state-checkbox/src/lib/components/next-three-state-checkbox/next-three-state-checkbox.component.spec.ts
@@ -1,9 +1,9 @@
-import {async, ComponentFixture, TestBed, fakeAsync, flush} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NextThreeStateCheckboxComponent} from './next-three-state-checkbox.component';
 import {By} from '@angular/platform-browser';
 import {Component, DebugElement} from '@angular/core';
-import {NgModel, FormsModule} from '@angular/forms';
+import {NgModel, FormsModule, FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 describe('ThreeStateCheckboxComponent', () => {
   let component: NextThreeStateCheckboxComponent;
@@ -27,14 +27,6 @@ describe('ThreeStateCheckboxComponent', () => {
 
   it('should generate id for element automatically', () => {
     expect(component.inputId.startsWith('next-three-state-checkbox-')).toBeTruthy();
-  });
-
-  it('should change value by click', () => {
-    const input = fixture.debugElement.query(By.css('input')).nativeElement;
-    expect(input.checked).toBeFalsy();
-    input.click();
-    fixture.detectChanges();
-    expect(input.checked).toBeTruthy();
   });
 });
 
@@ -62,8 +54,8 @@ describe('NextThreeStateCheckboxComponent with external id', () => {
 describe('ngModel', () => {
   let component: NextThreeStateCheckboxWithNgModel;
   let fixture: ComponentFixture<NextThreeStateCheckboxWithNgModel>;
-  let toggleDebugElement: DebugElement;
-  let toggleInstance: NextThreeStateCheckboxComponent;
+  let checkboxDebugElement: DebugElement;
+  let checkboxInstance: NextThreeStateCheckboxComponent;
   let ngModel: NgModel;
 
   beforeEach(async(() => {
@@ -76,14 +68,40 @@ describe('ngModel', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NextThreeStateCheckboxWithNgModel);
     fixture.detectChanges();
-    toggleDebugElement = fixture.debugElement.query(By.directive(NextThreeStateCheckboxComponent));
-    toggleInstance = toggleDebugElement.componentInstance;
-    ngModel = toggleDebugElement.injector.get<NgModel>(NgModel);
+    checkboxDebugElement = fixture.debugElement.query(By.directive(NextThreeStateCheckboxComponent));
+    checkboxInstance = checkboxDebugElement.componentInstance;
+    ngModel = checkboxDebugElement.injector.get<NgModel>(NgModel);
     component = fixture.componentInstance;
   });
 
+  it('should change state by click', () => {
+    const input = fixture.debugElement.query(By.css('input'));
+    const label = fixture.debugElement.query(By.css('label')).nativeElement;
+
+    fixture.whenStable().then(() => {
+      expect(checkboxInstance.checked).toBe(0);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeFalsy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(1);
+      expect(input.nativeElement.indeterminate).toBeTruthy();
+      expect(input.nativeElement.checked).toBeFalsy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(2);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeTruthy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(0);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeFalsy();
+    });
+  });
+
   it('should call writeVaue', () => {
-    const spyOnWriteValue = spyOn(toggleInstance, 'writeValue');
+    const spyOnWriteValue = spyOn(checkboxInstance, 'writeValue');
     fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(spyOnWriteValue).toHaveBeenCalled();
@@ -91,7 +109,7 @@ describe('ngModel', () => {
   });
 
   it('should call setDisabled', () => {
-    const spyOnSetDisabledState = spyOn(toggleInstance, 'setDisabledState');
+    const spyOnSetDisabledState = spyOn(checkboxInstance, 'setDisabledState');
     component.disabled = !component.disabled;
     fixture.detectChanges();
     fixture.whenStable().then(() => {
@@ -100,53 +118,128 @@ describe('ngModel', () => {
   });
 
   it('should change disabled var', () => {
-    toggleInstance.setDisabledState(true);
+    checkboxInstance.setDisabledState(true);
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      expect(toggleInstance.disabled).toBeTruthy();
+      expect(checkboxInstance.disabled).toBeTruthy();
     });
   });
 
   it('should react on click', () => {
     const input = fixture.debugElement.query(By.css('input')).nativeElement;
-    const spyOnRegisterChange = spyOn(toggleInstance, 'registerOnChange');
-    const spyOnRegisterOnTouched = spyOn(toggleInstance, 'registerOnTouched');
+    const spyOnRegisterOnChange = spyOn(checkboxInstance, 'registerOnChange');
+    const spyOnRegisterOnTouched = spyOn(checkboxInstance, 'registerOnTouched');
     input.click();
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      expect(spyOnRegisterChange).toHaveBeenCalled();
+      expect(spyOnRegisterOnChange).toHaveBeenCalled();
       expect(spyOnRegisterOnTouched).toHaveBeenCalled();
     });
   });
+});
 
-  it('should be disabled', async(
-    fakeAsync(() => {
-      const onchange = spyOn(toggleInstance, 'onChange');
-      toggleInstance.disabled = true;
-      toggleInstance.setDisabledState(true);
-      const input = fixture.debugElement.query(By.css('input'));
-      input.triggerEventHandler('change', new Event('change'));
+describe('Reactive form', () => {
+  let component: NextThreeStateCheckboxWithReactiveForm;
+  let fixture: ComponentFixture<NextThreeStateCheckboxWithReactiveForm>;
+  let checkboxDebugElement: DebugElement;
+  let checkboxInstance: NextThreeStateCheckboxComponent;
+  let spyOnWriteValue;
+  let spyOnRegisterOnChange;
+  let spyOnRegisterOnTouched;
+  let spyOnSetDisabledState;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [NextThreeStateCheckboxComponent, NextThreeStateCheckboxWithReactiveForm],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NextThreeStateCheckboxWithReactiveForm);
+    checkboxDebugElement = fixture.debugElement.query(By.directive(NextThreeStateCheckboxComponent));
+    checkboxInstance = checkboxDebugElement.componentInstance;
+    spyOnWriteValue = spyOn(checkboxInstance, 'writeValue');
+    spyOnRegisterOnChange = spyOn(checkboxInstance, 'registerOnChange');
+    spyOnRegisterOnTouched = spyOn(checkboxInstance, 'registerOnTouched');
+    spyOnSetDisabledState = spyOn(checkboxInstance, 'setDisabledState');
+
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should change state by click', async(() => {
+    const input = fixture.debugElement.query(By.css('input'));
+    const label = fixture.debugElement.query(By.css('label')).nativeElement;
+
+    fixture.whenStable().then(() => {
+      expect(checkboxInstance.checked).toBe(0);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeFalsy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(1);
+      expect(input.nativeElement.indeterminate).toBeTruthy();
+      expect(input.nativeElement.checked).toBeFalsy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(2);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeTruthy();
+
+      label.click();
+      expect(checkboxInstance.checked).toBe(0);
+      expect(input.nativeElement.indeterminate).toBeFalsy();
+      expect(input.nativeElement.checked).toBeFalsy();
+    });
+  }));
+
+  it('should call writeVaue', async(() => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
       fixture.detectChanges();
+      expect(spyOnWriteValue).toHaveBeenCalled();
+    });
+  }));
 
-      fixture.whenStable().then(() => {
-        flush();
-        fixture.detectChanges();
-        expect(onchange).not.toHaveBeenCalled();
-      });
-    }),
-  ));
+  it('should call setDisabled', () => {
+    component.reactiveForm.controls.threeStateCheckboxControl.disable();
+    checkboxInstance.disabled = !checkboxInstance.disabled;
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(spyOnSetDisabledState).toHaveBeenCalled();
+    });
+  });
+
+  it('should change disabled var', () => {
+    component.reactiveForm.controls.threeStateCheckboxControl.disable();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.reactiveForm.controls.threeStateCheckboxControl.disabled).toBeTruthy();
+    });
+  });
+
+  it('should react on click', () => {
+    const input = fixture.debugElement.query(By.css('input')).nativeElement;
+    input.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(spyOnRegisterOnChange).toHaveBeenCalled();
+      expect(spyOnRegisterOnTouched).toHaveBeenCalled();
+    });
+  });
 });
 
 @Component({
   template: `
-    <form class="ng-model-form">
+    <form>
       <next-three-state-checkbox
         [disabled]="disabled"
         [required]="required"
         [tabIndex]="tabIndex"
         [id]="id"
         [(ngModel)]="isChecked"
-        name="toggle"
+        name="checkbox"
       ></next-three-state-checkbox>
       <label for="1" class="checkbox-layout">reactive form</label>
     </form>
@@ -157,5 +250,28 @@ class NextThreeStateCheckboxWithNgModel {
   public required: boolean = true;
   public tabIndex: number = 1;
   public id: string = '1';
-  public isChecked: number = 2;
+  public isChecked: number = 0;
+}
+
+@Component({
+  template: `
+    <form [formGroup]="reactiveForm">
+      <next-three-state-checkbox
+        [required]="true"
+        [tabIndex]="1"
+        [id]="'2'"
+        formControlName="threeStateCheckboxControl"
+        name="checkbox2"
+      ></next-three-state-checkbox>
+      <label for="2">reactive form</label>
+    </form>
+  `,
+})
+class NextThreeStateCheckboxWithReactiveForm {
+  public required: boolean;
+  public tabIndex: number;
+  public id: string;
+  public reactiveForm = new FormGroup({
+    threeStateCheckboxControl: new FormControl({value: 0, disabled: false}),
+  });
 }
